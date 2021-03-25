449921d42ee1669c9f0fb62a25ef7dfae167134d
Tiago Neves <rykarumstuler@gmail.com>
2021-03-24 22:09:22 -0300

    Operadores

--- teste.kt
+++ teste.kt
@@ -1,7 +1,8 @@
 package com.example.teste
 
 fun main() {
-    execute()
+    //execute()
+    operadores()
 }
 
 fun execute() {
@@ -13,4 +14,27 @@ fun execute() {
     println(num1 * num2)
     println(num1 / num2)
     println(num1 % num2)
+}
+
+fun operadores() {
+
+    var num1 = 4
+    println(num1)
+
+    // Operadores de atribuição
+    num1 *= 5
+    println(num1)
+
+    // Operação de incremento
+    num1++
+    println(num1)
+    num1--
+    println(num1)
+    //É possível utilizar o incremento e decremento antes da variável ou depois, mas é preciso
+    //Saber que se estiver antes a variável modificar primeiro e depois retornar, se estiver depois
+    //a variável vai retornar o valor e depois modificar
+
+    // Operadores de comparações
+    val age = 21
+    println(age >= 18)
 }
