87cd1060d281da84c267cab72a9e6b8c0d88d828
Tiago Neves <rykarumstuler@gmail.com>
2021-03-24 21:36:33 -0300

    Primeiro código

---
+++ teste.kt
@@ -0,0 +1,16 @@
+package com.example.teste
+
+fun main() {
+    execute()
+}
+
+fun execute() {
+    val num1 = 10
+    val num2 = 20
+
+    println(num1 + num2)
+    println(num1 - num2)
+    println(num1 * num2)
+    println(num1 / num2)
+    println(num1 % num2)
+}
