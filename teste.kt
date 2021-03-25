06603e6bfbc305ac00f5d80c3484ce15e1d2161f
Tiago Neves <rykarumstuler@gmail.com>
2021-03-24 22:40:35 -0300

    Fim do conceito de input e do módulo: Conceitos básicos

--- teste.kt
+++ teste.kt
@@ -2,7 +2,9 @@ package com.example.teste
 
 fun main() {
     //execute()
-    operadores()
+    //operadores()
+    //input()
+    problem1()
 }
 
 fun execute() {
@@ -45,4 +47,35 @@ fun operadores() {
     * */
     val age = 21
     println(age >= 18)
+}
+
+fun input() {
+    println("Digite a sua idade:")
+    val age = readLine() // O valor de retorno é uma string
+
+    println("Sua idade é $age")
+
+    /* Para converter os valores do input para numérico é necessário colocar:
+    * !!.toInt() -> Esse dois pontos de exclamação é uma forma de validação, indica que o input
+    * não pode receber valores nulos e vazios
+    *
+    * !!.toDouble() -> Números reais
+    * */
+    println("Qual o preço do pão? ")
+    val pao = readLine()!!.toDouble()
+    println("O pão custa: $pao, é realmente muito caro")
+}
+
+fun problem1() {
+    // Criar um programa que informa quanto uma família gasta de água por ano
+    val diasNoAno = 365
+
+    println("Digite quanto é o gasto diário em Litros:")
+    val gastoDiario = readLine()!!.toDouble()
+
+    println("Digite a quantidade de anos:")
+    val years = readLine()!!.toInt()
+
+    val result = gastoDiario * years * diasNoAno
+    println("A quantidade de litros gastos em $years anos é: \n $result L")
 }
