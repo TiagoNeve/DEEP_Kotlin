9a70be7fea1336e3d8d0ec4c86b50f987faa99e9
Tiago Neves <rykarumstuler@gmail.com>
2021-03-24 22:59:25 -0300

    Fim do conceito de condicionais

--- controlFlow.kt
+++ controlFlow.kt
@@ -1,7 +1,8 @@
 package com.example.teste
 
 fun main() {
-    if01()
+    //if01()
+    con01()
 }
 
 fun if01() {
@@ -14,4 +15,25 @@ fun if01() {
         println("Proibido a entrada de maiores!")
     }
     // É possível ter muitas verificações, basta colocar um else if (condition)
+}
+
+fun con01() {
+    // Expressões condicionais, é o ato de você utilizar um if e guardar o resultado numa variável
+    // val validado = if (conta == conta && password == password) retorna true ou false
+
+    val x = 4
+    val y = if (x > 6) x-2 else x+2 // Expressões condicionais
+    println(x + y) // 10
+
+    println("Digite um número e verifique se é positivo ou negativo")
+    val num = readLine()!!.toInt()
+
+    val result = when {
+        num > 0 -> "Positivo"
+        num < 0 -> "Negativo"
+        else -> "Zero"
+    }
+    println(result)
+
+
 }
