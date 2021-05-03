aafc9d2d61d0292926aa704b940eeb73d948aae3
TiagoNeve <>
2021-05-03 22:57:54 +0000

    Fim da aula de Abstração

--- teste.kt
+++ teste.kt
@@ -1,3 +1,20 @@
 // Fundamentos de orientação a objetos em Kotlin - 
 
 // Introdução a abstrações
+/*
+  Abstrair é o ato de utilizar classes e heranças
+  para criar objetos que herdam atributos gerais.
+  Por exemplo, toda Pessoa possui corpo, mas pessoa possui
+  um corpo diferente, singular. Pode-se utilizar o mundo das ideias
+  para entender essa abstração. Imagina que o objeto ideal é a 
+  Abstração e o objeto real é a instância da abstração, com suas 
+  peculiaridades e defeitos.
+
+  Basicamente é isso, cria-se uma classe que possui atributos
+  gerais e depois vai implementando os objetos, assim se precisar
+  colocar uma nova funcionalidade a todos os objetos, não será
+  necessário colocar de um por um, basta introduzir na classe
+  e depois chamar nesse objeto, já que ele vai herdar todos os
+  atributos da classe pai.
+*/
+
